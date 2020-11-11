555efbe231b38fceba12e8aa977ae2c28dac1816
Jacob Marthaller <marthaller.jr@gmail.com>
2020-11-11 19:42:59 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,18 +1,55 @@
-def find_even_values(src)
-  # Given an Array of Arrays of Integers ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] ):
-  #
-  # Output all even values in each nested array
-   count = 0
-   while count < src.length do
-     nested_count = 0
-     while nested_count < src[count].length do
-       if src[count][nested_count] % 2 == 0
-       puts src[count][nested_count] 
-     end
-       nested_count += 1
-     end
-     count += 1
-   end
-end
+require 'pry'
+# def find_even_values(src)
+#   # Given an Array of Arrays of Integers ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] ):
+#   #
+#   # Output all even values in each nested array
+#   count = 0
+#   while count < src.length do
+#     nested_count = 0
+#     while nested_count < src[count].length do
+#       if src[count][nested_count] % 2 == 0
+#       puts src[count][nested_count] 
+#     end
+#       nested_count += 1
+#     end
+#     count += 1
+#   end
+# end
 
 #find_even_values([ [10, 11], [99, 50, 3, 4], [23, 41] ])
+
+
+
+
+
+
+
+
+
+
+
+
+
+def find_even_values(array)
+  count = 0
+  while count < array.length do 
+    inner_count = 0
+    while inner_count < array[count].length do 
+      if array[count][inner_count] % 2 == 0
+        print array[count][inner_count]
+      end
+      inner_count += 1
+    end
+    count += 1
+  end
+end
+
+
+
+
+
+
+
+
+
+
